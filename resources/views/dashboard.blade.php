b3a3407d7b275c5ba89159a9102ab88e57fa0e94
Ricardo <ricardosn606@gmail.com>
2026-09-02 21:45:57 -0400

    prueba 2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,7 @@
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path></svg>
                 Ver Estadísticas e Historial
                 <p>Desarrollado por daker 2/09</p>
+                <h1>2da prueba</h1>
             </a>
         </div>
     </x-slot>
